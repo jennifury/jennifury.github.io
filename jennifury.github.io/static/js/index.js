d07b9ae8603668c4ad75f385ecfe2de1196abe96
Jennifer Yang <42123788+jennifury@users.noreply.github.com>
2020-07-18 01:16:49 -0700

    Add files via upload

---
+++ jennifury.github.io/static/js/index.js
@@ -0,0 +1,88 @@
+function line(particle, particle2) {
+  context.beginPath();
+  context.moveTo(particle.x, particle.y);
+  context.lineTo(particle2.x, particle2.y);
+  context.stroke();
+}
+//function to conect code
+
+//animation for the code to move
+function animate() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < maxParticles; i++) {
+    let particle = particles[i];
+    context.fillRect(particle.x - particleSize / 2, particle.y - particleSize / 2, particleSize, particleSize);
+    for (let j = 0; j < maxParticles; j++) {
+      if (i != j) {
+        let particle2 = particles[j];
+        let distanceX = Math.abs(particle.x - particle2.x);
+        let distanceY = Math.abs(particle.y - particle2.y);
+        if (distanceX < threshold && distanceY < threshold) {
+          context.lineWidth = ((threshold * 1.2) - (distanceX + distanceY)) / 50;
+          if(j%2 ==0){
+            context.strokeStyle = 'rgb(204,204,255)';
+        }else{
+            context.strokeStyle = 'rgb(255, 102, 102)';
+        }
+          line(particle, particle2);
+        }
+      }
+    }
+    particle.x = particle.x + particle.vx;
+    particle.y = particle.y + particle.vy;
+    if (particle.x > canvas.width - particleSize || particle.x < particleSize) {
+      particle.vx = -particle.vx;
+    }
+    if (particle.y > canvas.height - particleSize || particle.y < particleSize)	 {
+      particle.vy = -particle.vy;
+    }
+  }
+  window.requestAnimationFrame(animate);
+}
+
+let canvas = document.getElementById('myCanvas');
+let context = canvas.getContext('2d');
+let particles = [];
+let particleSize = 3;
+let maxParticles =150;
+let threshold = 50;
+for (let i = 0; i < maxParticles; i++) {
+  let particle = {
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    vx: Math.random(),
+    vy: Math.random()
+  }
+
+  particles.push(particle);
+}
+context.fillStyle = 'white';
+animate();
+
+function msg() {
+  var msg="Thank you for your support!\n";
+  var quote ="\n'Life does not get better by chance. It gets better by change.'\n";
+  var q= "\n                                                       - Jim Rohn -";
+  alert(msg+quote+q);
+}
+
+
+
+function submitForm() {
+  document.getElementById("myform").reset();
+    alert("Message have been submitted");
+
+   }
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
